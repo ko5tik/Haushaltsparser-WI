46ae60da46fa17ecd2cfdbb94b091ef0c0f32cc1
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2011-11-25 16:26:13 +0100

    taps and siks total size computed

--- src/main/resources/inflow_code.js
+++ src/main/resources/inflow_code.js
@@ -1,18 +1,61 @@
 var renderFlow = function(config, flowData) {
 
+
     var r = Raphael("chart", config.width, config.height);
 
-    // iterate over taps and compute total amount
+    // iterate over taps and compute total amount  for each of them
+    computeBinTotals(config.taps, flowData);
+    // iterate through sinks and compute total amount for each of them
+    computeBinTotals(config.sinks, flowData);
+
+
+    //  compute total amount for sinks and taps, to calculate actual sizes
+    var totalTapSizes = 0;
+    var amountOfTaps = 0;
     for (var tapIndex in config.taps) {
         var tap = config.taps[tapIndex];
-        tap.total = 0;
-        for (var flowIndex  = 0; flowIndex < flowData.length; flowIndex++) {
-            var entry = flowData[flowIndex];
-
-            tap.total +=   entry[tap.out];
+        if (tap.total) {
+            totalTapSizes += tap[total];
+            amountOfTaps++;
         }
     }
 
+    var totalSinkSizes = 0;
+    var amountIfSinks = 0;
+    for (sink in config.sinks) {
+        if (sink.total) {
+            totalSinkSizes += sink.total;
+            amountIfSinks++;
+        }
+    }
+
+// now compute individual tap sizes and positions, taking tap spacing into account
+    var totalTapArea = totalTapSizes - config.bucketSpacing * (amountOfTaps - 1);
+    var top = 0;
+
+    for (tap in config.taps) {
+        tap.top = top;
+        tap.height = tap.total * totalTapArea / totalTapSizes;
+        top = top + tap.height + config.bucketSpacing;
+    }
+
+
+    //////////////////////////////////////////////////////////
+    // computes total amount for indivudual bins accumulating values from
+    // supplied data
+    function computeBinTotals(taps, flowData) {
+        for (var tapIndex in taps) {
+            var tap = taps[tapIndex];
+            tap.total = 0;
+            for (var flowIndex = 0; flowIndex < flowData.length; flowIndex++) {
+                var entry = flowData[flowIndex];
+                if (entry[tap.out]) {
+                    tap.total += entry[tap.out];
+                }
+            }
+        }
+
+    }
 };
 
 
