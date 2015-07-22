7fb59629d70b0de1bccb7e2e8bbd3b6c33bbad3b
ko5tik <konstantin.oribluda@gmail.com>
2015-07-22 23:38:53 +0200

    script works and renders charts

--- graphs/js/Accounts.js
+++ graphs/js/Accounts.js
@@ -35,4 +35,97 @@ function makeGraphs(error, apiData) {
         return d.year;
     });
 
+    var title = ndx.dimension(function (d) {
+        return d.title;
+    })
+
+
+    var yearIncome = year.group().reduceSum(function (d) {
+        // if ( d.accountId == 1100196 && 'Ergebnis' == d.qualifier && d.value < 0 ) {
+        //     console.log(d.year + ' ' + d.title + ' '+d.value + ' ' + d.parent + ' ' + d.amt)
+        //}
+        return ('Ergebnis' == d.qualifier && d.value < 0) ? -d.value : 0;
+    });
+
+    var yearSpending = year.group().reduceSum(function (d) {
+        return ('Ergebnis' == d.qualifier && d.value > 0) ? d.value : 0;
+    });
+
+
+    var entityGroup = entity.group();
+    var amtGroup = amt.group();
+    var dezernatGroup = dezernat.group();
+
+    var titleTotal = title.group().reduceSum(function (d) {
+        return -d.value;
+    });
+
+
+
+    amtField = dc.selectMenu('#amt')
+        .dimension(amt)
+        .group(amtGroup);
+
+    dezernatField = dc.selectMenu('#dezernat')
+        .dimension(dezernat)
+        .group(dezernatGroup);
+
+    entityField = dc.selectMenu('#entity')
+        .dimension(entity)
+        .group(entityGroup);
+
+    var entitySpendingChart = dc.barChart("#entity-spending-chart");
+    var entityEaringChart = dc.barChart("#entity-earning-chart");
+    var positionsChart = dc.rowChart("#entity-positions-chart");
+
+
+
+    entitySpendingChart
+        .xAxisLabel("Year")
+        .height(300)
+        .margins({top: 10, right: 10, bottom: 30, left: 50})
+        .centerBar(false)
+        .gap(5)
+        .dimension(year)
+        .group(yearSpending)
+        .x(d3.scale.ordinal().domain(year))
+        .elasticY(true)
+        .xUnits(dc.units.ordinal)
+        .renderHorizontalGridLines(true)
+        .renderVerticalGridLines(true)
+        .yAxis().tickFormat(d3.format("s")).ticks(6);
+
+    entityEaringChart
+        .x(d3.scale.ordinal().domain(year))
+        .xAxisLabel("Year")
+        .height(300)
+        .margins({top: 10, right: 10, bottom: 30, left: 50})
+        .centerBar(false)
+        .gap(5)
+        .dimension(year)
+        .group(yearIncome)
+        .x(d3.scale.ordinal().domain(year))
+        .elasticY(true)
+        .xUnits(dc.units.ordinal)
+        .renderHorizontalGridLines(true)
+        .renderVerticalGridLines(true)
+        .yAxis().tickFormat(d3.format("s")).ticks(6);
+
+
+
+    positionsChart
+        .height(600)
+        .transitionDuration(500)
+        .dimension(title)
+        .group(titleTotal)
+        .margins({top: 10, right: 50, bottom: 30, left: 50})
+
+        .gap(5)
+        .ordering(function (d) {
+            return d.value;
+        })
+        .elasticX(true)
+        .xAxis().tickFormat(d3.format("s"));
+
+    dc.renderAll();
 }
